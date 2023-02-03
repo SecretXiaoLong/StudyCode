cca188294b16d0e4ca9bae9ffc1b0e4d5cad1ba0
ShuangLong <secretxiaolong@163.com>
2023-02-03 11:24:04 +0800

    third commit

--- src/GitSecond.java
+++ src/GitSecond.java
@@ -2,6 +2,6 @@ public class GitSecond {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git2");
-
+        System.out.println("Hello Git3");
     }
 }
